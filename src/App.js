4ee71f3248c57c010f70025ae7a8603bde0cfee0
SaifullaJubair <saifullajubair6@gmail.com>
2023-07-22 02:08:03 +0600

    Post, delete

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import Home from "./components/Home/Home";
 import "./App.css";
+import { Toaster } from "react-hot-toast";
 
 function App() {
   return (
     <div className="App">
       <Home></Home>
+      <Toaster></Toaster>
     </div>
   );
 }
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,13 +1,52 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { toast } from "react-hot-toast";
 
 const Home = () => {
+  const [tasks, setTasks] = useState([]);
+  const [refresh, setRefresh] = useState(false);
   const handleSubmit = (event) => {
     event.preventDefault();
+    const date = new Date().toUTCString();
     const task = {
       task: event.target.task.value,
+      time: date,
     };
-    console.log(task);
+    fetch("http://localhost:5000/task", {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify(task),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        if (data.acknowledged) {
+          setRefresh(!refresh);
+          toast.success("Task Add Successfully");
+        }
+      });
   };
+
+  useEffect(() => {
+    fetch("http://localhost:5000/task")
+      .then((res) => res.json())
+      .then((data) => setTasks(data));
+  }, [refresh]);
+
+  const handleDelete = (tasks) => {
+    // console.log(tasks._id);
+    fetch(`http://localhost:5000/task/${tasks._id}`, {
+      method: "DELETE",
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        if (data.acknowledged) {
+          setRefresh(!refresh);
+          toast.success("successfully task delete");
+        }
+      });
+  };
+
   return (
     <div>
       <h1 className="text-4xl font-semibold my-20">Welcome To Todo List</h1>
@@ -28,6 +67,37 @@ const Home = () => {
         </label>
         <button className="">Add</button>
       </form>
+
+      <div>
+        <h1> Your all task {tasks.length}</h1>
+
+        {tasks.map((task) => (
+          <div key={task._id}>
+            <div className="w-1/4 mx-auto shadow-xl rounded-md p-4 my-8">
+              <div className="">
+                <h3>{task?.task}</h3>
+                <div className="flex my-4  ">
+                  <div className="mx-auto">
+                    <button className=" mx-2 bg-cyan-400 px-2 py-1 rounded-lg  text-white font-semibold">
+                      Update
+                    </button>
+                    <button
+                      className=" bg-red-600  px-2 py-1 rounded-lg  text-white font-semibold"
+                      onClick={() => handleDelete(task)}
+                    >
+                      Delete
+                    </button>
+                    <button className=" mx-2  px-2 py-1 rounded-lg  bg-green-400  text-white font-semibold">
+                      Complete
+                    </button>
+                  </div>
+                </div>
+                <p>{task?.time?.slice(0, 16)}</p>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
