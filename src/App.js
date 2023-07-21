3ef830aaba2a058a209ac5ea768f0ab10f0e7d92
SaifullaJubair <saifullajubair6@gmail.com>
2023-07-21 23:16:34 +0600

    creating folder structure

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import logo from "./logo.svg";
+import Home from "./components/Home/Home";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <h1>hello</h1>
+      <Home></Home>
     </div>
   );
 }
